e9fa1093fd762fde165f15ff655b460795b2631b
Juan José Salvador Piedra <juanjosalvador@netc.eu>
2018-03-02 01:26:30 +0100

    Update MainController.lua

--- MainController.lua
+++ MainController.lua
@@ -1,1 +1,7 @@
-
+function AchievementUnlockedHandler()
+    SendChatMessage("Congratulations!", "GUILD")
+end
+  
+local f = CreateFrame("frame")
+f:RegisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT")
+f:SetScript("OnEvent", AchievementUnlockedHandler()
